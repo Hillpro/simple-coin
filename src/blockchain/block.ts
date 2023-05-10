5b462efd3e321e19d263fc342def3cd15605eae0
Hillpro <66534835+Hillpro@users.noreply.github.com>
2023-05-10 15:22:52 -0400

    PoW

--- src/blockchain/block.ts
+++ src/blockchain/block.ts
@@ -7,25 +7,79 @@ export class Block {
   timestamp: number;
   data: Data;
   hash: string;
+  difficulty: number;
+  nonce: number;
 
-  constructor(index: number, previousHash: string, timestamp: number, data: Data) {
+  constructor(
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: Data,
+    difficulty: number,
+    nonce = 0,
+  ) {
     this.index = index;
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
+
     this.hash = this.calculateHash();
+    this.findNonceAndHash();
+  }
+
+  private findNonceAndHash() {
+    while (!Block.hashMatchesDifficulty(this.hash, this.difficulty)) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
   }
 
   calculateHash() {
-    return Block.calculateHash(this.index, this.previousHash, this.timestamp, this.data);
+    return Block.calculateHash(
+      this.index,
+      this.previousHash,
+      this.timestamp,
+      this.data,
+      this.difficulty,
+      this.nonce,
+    );
   }
 
   static calculateHashForBlock(block: Block) {
-    return Block.calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+    return Block.calculateHash(
+      block.index,
+      block.previousHash,
+      block.timestamp,
+      block.data,
+      block.difficulty,
+      block.nonce,
+    );
   }
 
-  static calculateHash(index: number, previousHash: string, timestamp: number, data: Data) {
-    return SHA256(index + previousHash + timestamp + data).toString();
+  static calculateHash(
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: Data,
+    difficulty: number,
+    nonce: number,
+  ) {
+    return SHA256(index + previousHash + timestamp + data + difficulty + nonce).toString();
+  }
+
+  static hashMatchesDifficulty(hash: string, difficulty: number) {
+    const binaryHash = parseInt(hash, 16).toString(2).padStart(4, '0');
+    const requiredPrefix = '0'.repeat(difficulty);
+    return binaryHash.startsWith(requiredPrefix);
+  }
+
+  static isTimestampValid(block: Block, previousBlock: Block) {
+    return (
+      previousBlock.timestamp - 60000 < block.timestamp &&
+      block.timestamp - 60000 < new Date().getTime()
+    );
   }
 
   static isBlockStructureValid(block: Block) {
--- src/blockchain/blockchain.ts
+++ src/blockchain/blockchain.ts
@@ -1,6 +1,12 @@
 import { Block } from './block';
 import { Data } from './data';
 
+// in seconds
+const BLOCK_GENERATION_INTERVAL = 10;
+
+// in blocks
+const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
+
 export class BlockChain {
   private blockchain: Block[];
 
@@ -10,7 +16,7 @@ export class BlockChain {
 
   static get genesisBlock() {
     // hash:"d00ff3db391778c7893b9c804c0f2d473edbe7649f21b318c349f228dbd5b57e"
-    return new Block(0, '0', 1680546496366, new Data('First block'));
+    return new Block(0, '0', 1680546496366, new Data('First block'), 0);
   }
 
   get latestBlock() {
@@ -21,11 +27,40 @@ export class BlockChain {
     return this.blockchain;
   }
 
+  get length() {
+    return this.blockchain.length;
+  }
+
+  get difficulty() {
+    if (
+      this.latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 &&
+      this.latestBlock.index !== 0
+    ) {
+      return this.adjustedDifficulty;
+    } else {
+      return this.latestBlock.difficulty;
+    }
+  }
+
+  private get adjustedDifficulty() {
+    const prevAdjustmentBlock = this.blockchain[this.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+    const timeExpected = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+    const timeTaken = this.latestBlock.timestamp - prevAdjustmentBlock.timestamp;
+
+    if (timeTaken < timeExpected / 2) {
+      return prevAdjustmentBlock.difficulty + 1;
+    } else if (timeTaken > timeExpected * 2) {
+      return prevAdjustmentBlock.difficulty - 1;
+    } else {
+      return prevAdjustmentBlock.difficulty;
+    }
+  }
+
   generateNextBlock(blockData: Data) {
     const previousBlock = this.latestBlock;
     const nextIndex = previousBlock.index + 1;
     const nextTimestamp = new Date().getTime();
-    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, this.difficulty);
   }
 
   addBlock(newBlock: Block) {
@@ -49,15 +84,33 @@ export class BlockChain {
     } else if (previousBlock.hash !== newBlock.previousHash) {
       console.log('Invalid previousHash');
       return false;
+    } else if (Block.isTimestampValid(newBlock, previousBlock)) {
+      console.log('Invalid timestamp');
+      return false;
     } else if (Block.calculateHashForBlock(newBlock) !== newBlock.hash) {
       console.log('Invalid hash');
       return false;
+    } else if (Block.hashMatchesDifficulty(newBlock.hash, newBlock.difficulty)) {
+      console.log("Hash doesn't match difficulty");
+      return false;
     }
+
     return true;
   }
 
+  get accumulatedDifficulty() {
+    return BlockChain.accumulatedDifficulty(this.blockchain);
+  }
+
+  static accumulatedDifficulty(blocks: Block[]) {
+    return blocks.map((block) => Math.pow(2, block.difficulty)).reduce((a, b) => a + b);
+  }
+
   replaceChain(newBlocks: Block[]) {
-    if (BlockChain.isValidChain(newBlocks) && newBlocks.length > this.blockchain.length) {
+    if (
+      BlockChain.isValidChain(newBlocks) &&
+      BlockChain.accumulatedDifficulty(newBlocks) > this.accumulatedDifficulty
+    ) {
       console.log(
         'Received blockchain is valid. Replacing current blockchain with received blockchain',
       );
@@ -73,15 +126,13 @@ export class BlockChain {
     if (JSON.stringify(blocks[0]) !== JSON.stringify(this.genesisBlock)) {
       return false;
     }
-    const tempBlocks = [blocks[0]];
 
     for (let i = 1; i < blocks.length; i++) {
-      if (BlockChain.isBlockValid(blocks[i], tempBlocks[i - 1])) {
-        tempBlocks.push(blocks[i]);
-      } else {
+      if (!BlockChain.isBlockValid(blocks[i], blocks[i - 1])) {
         return false;
       }
     }
+
     return true;
   }
 }
